148da97ecfaed417e1fad0dc8d7d45af631e846b
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-05 20:21:31 +0700

    ✨ feat: add onboarding contents as an personal constant file

---
+++ constants/contents.ts
@@ -0,0 +1,9 @@
+export const GlobalContents = {
+    onboardingContents: [
+        {
+            title: "Selamat Datang!",
+            subtitle: "Belajar Bahasa Indonesia dengan Cara Paling Efektif. Cukup dengan mendengarkan! Nikmati percakapan sehari- hari dari penutur asli dan tingkatkan pemahamanmu secara alami.",
+            image: require("../assets/Onboarding/onboarding_1.png"),
+        }
+    ]
+}

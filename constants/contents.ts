df419034d977eb7185a375879da5baefd9ca7d21
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-05 20:45:21 +0700

    ✨ feat: add more content to onboarding constant

--- constants/contents.ts
+++ constants/contents.ts
@@ -4,6 +4,16 @@ export const GlobalContents = {
             title: "Selamat Datang!",
             subtitle: "Belajar Bahasa Indonesia dengan Cara Paling Efektif. Cukup dengan mendengarkan! Nikmati percakapan sehari- hari dari penutur asli dan tingkatkan pemahamanmu secara alami.",
             image: require("../assets/Onboarding/onboarding_1.png"),
+        },
+        {
+            title: "Belajar dengan Mendengarkan!",
+            subtitle: "Dengarkan percakapan asli dan pahami maknanya dengan bantuan transkrip serta terjemahan",
+            image: require("../assets/Onboarding/onboarding_2.png"),
+        },
+        {
+            title: "Ayo Mulai!",
+            subtitle: "Pilih topik favoritmu dan mulai melatih kemampuan mendengarmu sekarang!",
+            image: require("../assets/Onboarding/onboarding_3.png"),
         }
     ]
 }

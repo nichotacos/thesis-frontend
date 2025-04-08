38626c5b0062d97b0aad5bab5ea49c8f4b9fa41c
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-08 21:20:30 +0700

    ✨ feat: create auth api file

---
+++ api/auth.api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const apiClient = axios.create({
+    baseURL: `${process.env.API_URL}`,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    timeout: 10000,
+});

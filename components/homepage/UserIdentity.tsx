dbefa51a86d4285b459f90d6a28b118a5c342a3b
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-11 15:13:23 +0700

    ✨ feat: add userIdentity component for the homescreen

---
+++ components/homepage/UserIdentity.tsx
@@ -0,0 +1,92 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+import getFirstName from "../../utils/getFirstName";
+
+interface UserIdentityProps {
+    profilePicture?: string;
+    userFullName: string;
+    userLevel: number;
+    totalGems: number;
+}
+
+export default function UserIdentity({
+    profilePicture,
+    userFullName,
+    userLevel,
+    totalGems,
+}: UserIdentityProps) {
+    const name = getFirstName(userFullName);
+
+    return (
+        <View style={styles.innerContainer}>
+            <View style={styles.innerContainer}>
+                <Image
+                    source={require("../../assets/default-avatar.jpeg")}
+                    style={styles.avatarImage}
+                />
+                <View style={styles.textContainer}>
+                    <Text style={styles.haloText}>Halo, <Text style={styles.nameText}>{name}</Text></Text>
+                    <Text style={styles.levelText}>{`Level ${userLevel}`}</Text>
+                </View>
+            </View>
+            <View style={styles.gemsContainer}>
+                <Text style={styles.gemsCountText}>{totalGems}</Text>
+                <Image
+                    source={require("../../assets/gamification/gems.png")}
+                    style={styles.gemsImage}
+                />
+            </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    innerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 4
+    },
+    avatarImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+    },
+    textContainer: {
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        marginLeft: 16,
+    },
+    haloText: {
+        fontSize: 20,
+        fontFamily: "Inter-Bold",
+    },
+    nameText: {
+        fontSize: 20,
+        fontFamily: "Inter-Bold",
+        color: GlobalStyles.colors.primary,
+    },
+    levelText: {
+        fontSize: 14,
+        fontFamily: "Inter-Regular",
+    },
+    gemsContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: GlobalStyles.colors.primary,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 8,
+        gap: 8,
+    },
+    gemsCountText: {
+        fontSize: 20,
+        fontFamily: "Inter-Bold",
+        color: "white",
+    },
+    gemsImage: {
+        width: 30,
+        height: 30,
+    }
+})

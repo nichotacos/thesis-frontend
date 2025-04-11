7f60e8f6fffde5d0d8873d64664fe8eb61918859
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-11 17:44:40 +0700

    💄 feat: add more colors in constant file

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,8 +1,10 @@
 export const GlobalStyles = {
     colors: {
         primary: "#A60000",
+        lighterPrimary: "#DF8080",
         accent: "#F9A825",
         lighterAccent: "#FFC466",
+        lighterAccent2: "#FFE6BD",
         whiteFont: "#FFFFFF",
         lightBackground: "#F2F7FB",
         lightBlue: "#94BCE5"

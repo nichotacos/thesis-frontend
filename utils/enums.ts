990b85fb734308672aec9313e571896a26b1b03b
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-13 20:35:43 +0700

    ✨ feat(utils): add level enum

---
+++ utils/enums.ts
@@ -0,0 +1,27 @@
+
+export const Levels = [
+    {
+        name: "Bali",
+        level: 'a1',
+    },
+    {
+        name: 'Jakarta',
+        level: 'a2',
+    },
+    {
+        name: 'Surabaya',
+        level: 'b1',
+    },
+    {
+        name: 'Aceh',
+        level: 'b2',
+    },
+    {
+        name: 'Padang',
+        level: 'c1',
+    },
+    {
+        name: 'Bandung',
+        level: 'c2',
+    },
+]

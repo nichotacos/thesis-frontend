06008f85d044ab1868255f0af4e2bd5521e8edc5
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-13 20:38:57 +0700

    ✨ feat: add navigation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,8 +3,11 @@ import { GlobalStyles } from "../constants/styles";
 import UserIdentity from "../components/homepage/UserIdentity";
 import StreakContainer from "../components/homepage/StreakContainer";
 import LearningWidget from "../components/homepage/LearningWidget";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+    const navigation = useNavigation();
+
     return (
         <ScrollView style={styles.container} contentContainerStyle={styles.scrollViewContainer}>
             <UserIdentity
@@ -21,12 +24,16 @@ export default function HomeScreen() {
                 <LearningWidget
                     wordsLearned={100}
                     isDictionary={true}
+                    onPress={() => { }}
                 />
                 <LearningWidget
                     userSkillLevel="Jakarta"
                     userLatestModule={2}
                     userLatestModuleName="Hati-Hati di Jalan"
                     isDictionary={false}
+                    onPress={() => {
+                        navigation.navigate("LevelScreen")
+                    }}
                 />
             </View>
         </ScrollView>

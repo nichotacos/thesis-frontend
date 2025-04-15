92ce39e66d07a9e11a8eb12a815d750df6b3633f
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-16 00:06:39 +0700

    🏷️ feat: create User type

---
+++ types/User.ts
@@ -0,0 +1,18 @@
+export type User = {
+    id: string;
+    username: string;
+    userFullName: string;
+    email: string;
+    totalExp: number;
+    weeklyExp: number;
+    dailyExp: number;
+    totalStreak: number;
+    highestStreak: number;
+    totalGems: number;
+    lastLogin?: Date;
+    profilePicture?: string;
+    currentLevel?: string;
+    currentModule?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}

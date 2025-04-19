4ca2ab3082b9f1ed5182d2bcee60225a6adc20e5
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-19 20:59:16 +0700

    ✨ feat: show level path

--- screens/learning/LevelScreen.tsx
+++ screens/learning/LevelScreen.tsx
@@ -1,62 +1,101 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import LevelStamp from "../../components/learning/LevelStamp";
 import Svg, { Path } from "react-native-svg";
+import { useEffect, useState } from "react";
+import { apiClient } from "../../api/apiClient";
+import { Level } from "../../types/Level";
+import { useSelector } from "react-redux";
+import { User } from "../../types/User";
+import { GlobalContents } from "../../constants/contents";
 
 interface LevelScreenProps {
-    userCurrentLevel: string;
     route: any;
 }
 
 export default function LevelScreen({
-    userCurrentLevel,
     route
 }: LevelScreenProps) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.content}>
-                <LevelStamp
-                    imageSource={require('../../assets/gamification/bali-stamp.png')}
-                    name="Bali"
-                    style={{ top: 40, left: 20 }}
-                />
-                <Svg viewBox="0 0 630 1200" width="100%" height="100%" style={{ borderWidth: 1 }}>
-                    <Path
-                        d="M243.584228515625,102.09318542480469C297.1923522949219,105.64515686035156,469.38470458984375,75.47789255777995,515.2329711914062,103.40501403808594C561.0812377929688,131.33213551839194,526.4336853027344,245.2807642618815,528.673828125,320.6559143066406"
-                        fill="none"
-                        stroke={GlobalStyles.colors.primary}
-                        strokeWidth={20}
-                        strokeLinecap="round"
-                    />
-                </Svg>
+    const [levels, setLevels] = useState<Level[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const userData = useSelector((state: { user: { userInfo: Partial<User> } }) => state.user.userInfo);
 
-                <LevelStamp
-                    imageSource={require('../../assets/gamification/jakarta-stamp.png')}
-                    name="Jakarta"
-                    style={{ top: 235, right: 20 }}
-                />
+    const stampPosition = {
+        top: 40,
+        left: 20,
+        right: 20,
+    }
 
-                <Svg viewBox="0 0 620 1200" width="100%" height="100%" style={{ borderWidth: 1, position: "absolute", top: 210, left: 20 }}>
-                    <Path
-                        d="M243.584228515625,102.09318542480469C297.1923522949219,105.64515686035156,469.38470458984375,75.47789255777995,515.2329711914062,103.40501403808594C561.0812377929688,131.33213551839194,526.4336853027344,245.2807642618815,528.673828125,320.6559143066406"
-                        fill="none"
-                        stroke={GlobalStyles.colors.primary}
-                        strokeWidth={20}
-                        strokeLinecap="round"
-                        transform={[
-                            { scaleX: -1 },
-                            { translateX: 620 } // adjust based on viewBox width
-                        ]}
-                    />
-                </Svg>
+    useEffect(() => {
+        const fetchLevels = async () => {
+            try {
+                setIsLoading(true);
+                const response = await apiClient.get('/level');
+                setLevels(response.data.levels);
+            } catch (error) {
+                console.error('Error fetching levels:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
 
-                <LevelStamp
-                    imageSource={require('../../assets/gamification/surabaya-stamp.png')}
-                    name="Surabaya"
-                    style={{ top: 440, left: 20 }}
-                />
+        fetchLevels();
+    }, []);
+
+    if (isLoading || !userData) {
+        return (
+            <View style={styles.container}>
+                <Text>Loading...</Text>
             </View>
-        </View>
+        )
+    }
+
+    return (
+        <ScrollView
+            style={styles.container}
+            contentContainerStyle={[styles.content, { height: levels.length * 195 + 160 }]}
+        >
+            {levels.map((level, index) => {
+                const imageSource = GlobalContents.levelImages[index].image;
+                return (
+                    <View
+                        key={index}
+                    >
+                        <LevelStamp
+                            imageSource={imageSource}
+                            name={level.name}
+                            style={[
+                                index % 2 === 0 ? { left: 20 } : { right: 20 },
+                                { top: stampPosition.top + 195 * index },
+                                // { opacity: userData.currentLearnLevel.localeCompare(level.actualBipaLevel) ? 1 : 0.5 },
+                            ]}
+                        />
+                        {/* TODO: Atur path yang sesuai */}
+                        {index !== levels.length - 1 && (
+                            <Svg viewBox="0 0 1230 1500" width={680} height={700} style={{
+                                position: "absolute",
+                                top: 80 + 195 * index,
+                                left: index % 2 === 0 && -20,
+                                right: index % 2 !== 0 && -170,
+                            }}
+                            >
+                                <Path
+                                    d="M243.584228515625,102.09318542480469C297.1923522949219,105.64515686035156,469.38470458984375,75.47789255777995,515.2329711914062,103.40501403808594C561.0812377929688,131.33213551839194,526.4336853027344,245.2807642618815,528.673828125,320.6559143066406"
+                                    fill="none"
+                                    stroke={GlobalStyles.colors.primary}
+                                    strokeWidth={24}
+                                    strokeLinecap="round"
+                                    transform={[
+                                        { scaleX: index % 2 !== 0 && -1 },
+                                        { translateX: index % 2 !== 0 ? 900 : 0 },
+                                    ]}
+                                />
+                            </Svg>
+                        )}
+                    </View>
+                )
+            })}
+        </ScrollView>
     )
 };
 
@@ -67,8 +106,8 @@ const styles = StyleSheet.create({
         padding: 16
     },
     content: {
-        flex: 1,
         paddingHorizontal: 16,
         position: "relative",
+        backgroundColor: GlobalStyles.colors.lightBackground,
     }
 })

ef7de2ba6f5752899fd78915f6414f047ee63e4e
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-21 00:52:15 +0700

    ✨ feat: add register user api

---
+++ api/user/index.ts
@@ -0,0 +1,20 @@
+import { apiClient } from "../apiClient";
+
+interface RegisterPayload {
+    full_name: string;
+    username: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+    captchaToken: string;
+}
+
+export async function registerUser(user: RegisterPayload) {
+    try {
+        const response = await apiClient.post(`/user`, user);
+        return response.data;
+    } catch (error) {
+        console.error('Error registering user:', error);
+        throw error; // Rethrow the error for further handling if needed
+    }
+}

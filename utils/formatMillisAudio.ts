115ef8874c7c53d3ceae6fff53842bc88eacfa47
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-20 20:55:58 +0700

    ✨ feat(utils): add format milis to get duration of audio

---
+++ utils/formatMillisAudio.ts
@@ -0,0 +1,7 @@
+export const formatMillis = (millis: number | null) => {
+    if (!millis) return "0:00";
+    const totalSeconds = Math.floor(millis / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+};

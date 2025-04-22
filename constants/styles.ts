08b801a1f9c65802bde6e6884d687ef4257f32c7
Nicholas Yang <nicho.yeo7@gmail.com>
2025-04-23 02:31:46 +0700

    💄 feat: add more colors

--- constants/styles.ts
+++ constants/styles.ts
@@ -7,6 +7,9 @@ export const GlobalStyles = {
         lighterAccent2: "#FFE6BD",
         whiteFont: "#FFFFFF",
         lightBackground: "#F2F7FB",
-        lightBlue: "#94BCE5"
+        lightBlue: "#94BCE5",
+        success: "#4CAF50",
+        classicBlue: "#2196F3",
+
     },
 }

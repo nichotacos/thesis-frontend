0278729718bbb81f4d7d06eb0e419398f8efb2d1
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-18 18:04:14 +0700

    ✨ feat: add shop item type

---
+++ types/ShopItem.ts
@@ -0,0 +1,11 @@
+export type ShopItem = {
+    _id: string;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    category: string;
+    active: boolean;
+    createdAt: string;
+    updatedAt: string;
+}

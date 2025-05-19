f004d8f450d1eb07a057419915aa30057c6ca301
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-19 23:29:17 +0700

    ✨ feat: add modules on level type

--- types/Level.ts
+++ types/Level.ts
@@ -1,8 +1,11 @@
+import { Module } from "react-native";
+
 export type Level = {
     _id: string;
     name: string;
     actualBipaLevel: string;
     description: string;
+    modules: Module[];
     createdAt?: Date;
     updatedAt?: Date;
 }

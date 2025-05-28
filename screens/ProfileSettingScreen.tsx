8950055bb0aa6a6e6933710843cb567670f8c20e
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-28 17:35:02 +0700

    ✨ feat: add profile setting screen

---
+++ screens/ProfileSettingScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function ProfileSettingScreen(params) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Profile Setting Screen</Text>
+        </View>
+    )
+};

553b98eeccd43d324fc0fe448f1abd61c5c2a73c
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-30 18:19:43 +0700

    💄 feat: add more colors for styling

--- constants/styles.ts
+++ constants/styles.ts
@@ -1,15 +1,19 @@
 export const GlobalStyles = {
     colors: {
         primary: "#A60000",
+        darkerPrimary: "#6D0000",
+        primary2: "#c71a1a",
         lighterPrimary: "#DF8080",
+        lighterPrimary2: "#B35C5C",
         accent: "#F9A825",
+        darkerAccent: "#E65100",
         lighterAccent: "#FFC466",
         lighterAccent2: "#FFE6BD",
         whiteFont: "#FFFFFF",
         lightBackground: "#F2F7FB",
+        lightRedBackground: "#f7e1e1",
         lightBlue: "#94BCE5",
         success: "#4CAF50",
         classicBlue: "#2196F3",
-
     },
 }

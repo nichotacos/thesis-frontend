deff8e49fbfe61a6d96fbfe993f2c14f78cc4bde
Nicholas Yang <nicho.yeo7@gmail.com>
2025-05-30 20:12:52 +0700

    ✨ feat: add functionality to buy heart

--- components/learning/LevelModuleHeader.tsx
+++ components/learning/LevelModuleHeader.tsx
@@ -1,20 +1,39 @@
 import { View, Image, Pressable, Text, StyleSheet } from "react-native"
 import StreakFireSvg from "../../assets/gamification/streak-fire-svg"
 import HeartSVG from "../../assets/gamification/heart-svg"
+import { useState } from "react";
+import BuyHeartModal from "./BuyHeartModal";
+import { useSelector } from "react-redux";
 
 interface LevelModuleHeaderProps {
     totalGems: number;
     streakCount: number;
     heartCount: number;
+    earliestLostHeartTime: string | null;
 }
 
 export default function LevelModuleHeader({
     totalGems,
     streakCount,
     heartCount,
+    earliestLostHeartTime
 }: LevelModuleHeaderProps) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
     return (
         <View style={styles.header}>
+            <BuyHeartModal
+                isVisible={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onBuy={(quantity) => {
+                    console.log(`Bought ${quantity} hearts`);
+                    setIsModalOpen(false);
+                }}
+                availableHearts={heartCount}
+                earliestLostHeartTime={earliestLostHeartTime || null}
+            />
+
             <View style={styles.gemsOrStreakContainer}>
                 <Image
                     source={require('../../assets/gamification/gems.png')}
@@ -30,7 +49,13 @@ export default function LevelModuleHeader({
                 <StreakFireSvg width={40} height={40} />
                 <Text style={styles.gemsOrStreakText}>{streakCount}</Text>
             </View>
-            <Pressable onPress={() => { console.log('hearts pressed') }} style={styles.gemsOrStreakContainer}>
+            <Pressable
+                onPress={() => setIsModalOpen(true)}
+                style={[
+                    styles.gemsOrStreakContainer,
+                    // { borderColor: heartCount > 0 ? "#FFB6C1" : "#D3D3D3", borderWidth: 6, padding: 4, borderRadius: 12 }
+                ]}
+            >
                 <HeartSVG width={40} height={40} />
                 <Text style={styles.gemsOrStreakText}>{heartCount}</Text>
             </Pressable>

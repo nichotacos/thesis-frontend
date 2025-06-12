badc2c801de0d52537d840e55fc1e5ec6bd7143e
Nicholas Yang <nicho.yeo7@gmail.com>
2025-06-13 00:26:15 +0700

    ✨ feat: register verify otp screen

--- App.js
+++ App.js
@@ -24,6 +24,15 @@ import Toast from 'react-native-toast-message';
 import ProfileSettingScreen from './screens/ProfileSettingScreen';
 import EditProfileScreen from './screens/EditProfileScreen';
 import AchievementScreen from './screens/AchievementScreen';
+import OTPVerificationScreen from './screens/auth/VerifyOtpScreen';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { useCallback, useEffect, useState } from 'react';
+
+// SplashScreen.preventAutoHideAsync();
+// SplashScreen.setOptions({
+//   duration: 1000,
+//   fade: true,
+// });
 
 
 const Stack = createNativeStackNavigator();
@@ -221,6 +230,11 @@ function RootNavigator() {
             component={RegisterScreen}
             options={{ headerShown: false }}
           />
+          <Stack.Screen
+            name="VerifyOTPScreen"
+            component={OTPVerificationScreen}
+            options={{ headerShown: false }}
+          />
         </>
       )}
       {/* <Stack.Screen name="LevelScreen" component={LevelScreen} /> */}
@@ -229,11 +243,44 @@ function RootNavigator() {
 }
 
 export default function App() {
+  // const [appIsReady, setAppIsReady] = useState(false);
+
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       // Pre-load fonts, make any API calls you need to do here
+  //       // Artificially delay for two seconds to simulate a slow loading
+  //       // experience. Remove this if you copy and paste the code!
+  //       await new Promise(resolve => setTimeout(resolve, 2000));
+  //     } catch (e) {
+  //       console.warn(e);
+  //     } finally {
+  //       // Tell the application to render
+  //       setAppIsReady(true);
+  //       console.log('App is ready');
+  //     }
+  //   }
+
+  //   prepare();
+  // }, []);
+
+  // const onLayoutRootView = useCallback(() => {
+  //   if (appIsReady) {
+  //     SplashScreen.hide();
+  //   }
+  // }, [appIsReady]);
+
+  // if (!appIsReady) {
+  //   return null;
+  // }
+
   return (
     <SafeAreaView style={style.container}>
       <Provider store={store}>
         <CopilotProvider style={{ top: 16 }}>
-          <NavigationContainer>
+          <NavigationContainer
+          // onReady={onLayoutRootView}
+          >
             <RootNavigator />
             <Toast />
           </NavigationContainer>
